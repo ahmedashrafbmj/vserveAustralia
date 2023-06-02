6818e0ca0c667ec111e10302be0ce3f5d1e4247d
imranulhaqhnhtechsolutions <124700391+imranulhaqhnhtechsolutions@users.noreply.github.com>
2023-06-02 04:34:35 -0700

    checkong

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,11 @@ import dynamic from 'next/dynamic';
   
 import Loader from '@/common/Loader';
 const HomeTwo = dynamic(() => import('@/components/home/home-2/hometwo'), {
+  ssr: true,
   loading: () =><Loader/>
 });
 const SEO = dynamic(() => import('@/components/seo'), {
+  ssr: true,
   loading: () =><Loader/>
 });
 
