44ea9b02cd816be4415c3e33e7fa09259c28c46a
imranulhaqhnhtechsolutions <124700391+imranulhaqhnhtechsolutions@users.noreply.github.com>
2023-06-02 04:08:22 -0700

    checkong

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
-const withPurgeCss = require('next-purgecss');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
 };
 
-module.exports = withPurgeCss(nextConfig, {
-  purgeCss: {
-    content: [
-      './pages/**/*.{js,jsx,ts,tsx}',
-      './components/**/*.{js,jsx,ts,tsx}',
-    ],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-});
+module.exports = nextConfig;
+
+// const withPurgeCss = require('next-purgecss');
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+// };
+
+// module.exports = withPurgeCss(nextConfig, {
+//   purgeCss: {
+//     content: [
+//       './pages/**/*.{js,jsx,ts,tsx}',
+//       './components/**/*.{js,jsx,ts,tsx}',
+//     ],
+//     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+//   },
+// });
