edfa6522ee9e77d31179c7a1488e7d769764c458
imranulhaqhnhtechsolutions <124700391+imranulhaqhnhtechsolutions@users.noreply.github.com>
2023-06-02 03:43:00 -0700

    removing unused css

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const withPurgeCss = require('next-purgecss');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-
+  withPurgeCss: {
+    purgeCss: {
+      content: [
+        './pages/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+      ],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    },
+  },
 };
 
 module.exports = nextConfig;
